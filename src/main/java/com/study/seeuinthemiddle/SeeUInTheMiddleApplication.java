06840de1f4117259d6b23a244e363754119f121c
Moon <bbangssi91@gmail.com>
2021-09-12 16:54:42 +0900

    first commit

---
+++ src/main/java/com/study/seeuinthemiddle/SeeUInTheMiddleApplication.java
@@ -0,0 +1,13 @@
+package com.study.seeuinthemiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeUInTheMiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeeUInTheMiddleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/seeuinthemiddle/SeeUInTheMiddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.seeuinthemiddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeUInTheMiddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
